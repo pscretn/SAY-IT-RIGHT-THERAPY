f6c00c1c888e0fde2c7326a980d0678748232ffd
PAUL SABU <paulsabu.123@gmail.com>
2022-03-12 09:21:59 +0530

    python code

--- Voice_App.py
+++ Voice_App.py
@@ -2,30 +2,43 @@ import speech_recognition as sr
 import pyttsx3 as pyx
 import random
 import pronouncing as pr
+from pydub import AudioSegment
+from pydub.playback import play
 
 record = sr.Recognizer()
 hint1 = "Can you repeat after me , the word"
 words = "orange","strawberry","apple","man","boy","girl","woman","blue","red"
-
+startbeep =  AudioSegment.from_wav("sounds\startbeep.wav")
+stopbeep =  AudioSegment.from_wav("sounds\endbeep.wav")
 def SpeakText(voice):
     engine = pyx.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate)
     engine.say(voice)
     engine.runAndWait()
     engine.stop()
 
 def pronoun(word):
-    val=2
+    val=4
+    simword=False
     while(val>0):
-        print("you said the wrong word , you have "+str(val)+" chances left")
-        SpeakText("you said the wrong word , you have "+str(val)+" chances left")
+        if simword==False:
+            print("you said the wrong word , you have "+str(val)+" chances left")
+            SpeakText("you said the wrong word , you have "+str(val)+" chances left")
+        else:
+            simword=False
         try:
             with sr.Microphone() as mic:
+                SpeakText(hint1+word)
+                print(word)
                 record.adjust_for_ambient_noise(mic, duration=0.2)
+                play(startbeep)
                 print("Listening...")
                 audio = record.listen(mic)
-                print("Thinking...")
+                print("Recognizing...")
+                play(stopbeep)
                 text = record.recognize_google(audio)
                 text = text.lower()
                 test=False
@@ -41,7 +54,7 @@ def pronoun(word):
                     if(test):
                         print("You are closer to the word")
                         SpeakText("You are closer to the word")
-                        break
+                        simword=True
                     val-=1
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
@@ -65,9 +78,11 @@ def  UserVoice(word):
     try:
         with sr.Microphone() as mic:
             record.adjust_for_ambient_noise(mic, duration=0.2)
+            play(startbeep)
             print("Listening...")
             audio = record.listen(mic)
             print("Thinking...")
+            play(stopbeep)
             text = record.recognize_google(audio)
             text = text.lower()
             checkword(text,word)
@@ -75,9 +90,9 @@ def  UserVoice(word):
         print("Could not request results; {0}".format(e))
     except sr.UnknownValueError:
         print("unknown error occured")
-while(True):
-    word=random.choice(words)
-    print(word)
-    SpeakText(hint1+word)
-    UserVoice(word)
+
+word=random.choice(words)
+print(word)
+SpeakText(hint1+word)
+UserVoice(word)
        
