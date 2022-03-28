476095d0cf4fafc6655c0509638de06dbbc55e0e
pscretn <paulsabu.123@gmail.com>
2022-03-28 11:35:05 +0530

    file

--- Word_Pronouncing_RaspberryPI/start_pronoun.py
+++ Word_Pronouncing_RaspberryPI/start_pronoun.py
@@ -1,11 +1,73 @@
 import wiringpi
 import subprocess
+import time
 button =29
+r=28
+g=27
+b=26
 wiringpi.wiringPiSetup()
 wiringpi.pinMode(button,0)
+wiringpi.pinMode(r,1)
+wiringpi.pinMode(g,1)
+wiringpi.pinMode(b,1)
+#red
+wiringpi.digitalWrite(r,1)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,0)
+time.sleep(0.2)
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,0)
+#green
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,1)
+wiringpi.digitalWrite(b,0)
+time.sleep(0.2)
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,0)
+#blue
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,1)
+time.sleep(0.2)
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,0)
+#yellow
+wiringpi.digitalWrite(r,1)
+wiringpi.digitalWrite(g,1)
+wiringpi.digitalWrite(b,0)
+time.sleep(0.2)
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,0)
+#magenta
+wiringpi.digitalWrite(r,1)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,1)
+time.sleep(0.2)
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,0)
+#cyan
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,1)
+wiringpi.digitalWrite(b,1)
+time.sleep(0.2)
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,0)
+#white
+wiringpi.digitalWrite(r,1)
+wiringpi.digitalWrite(g,1)
+wiringpi.digitalWrite(b,1)
+time.sleep(0.2)
+wiringpi.digitalWrite(r,0)
+wiringpi.digitalWrite(g,0)
+wiringpi.digitalWrite(b,0)
 subprocess.call(['bash /home/pi/SAY-IT-RIGHT-THERAPY/Word_Pronouncing_RaspberryPI/pronouncing_system.sh'],shell = True)
 while(True):
-    #print(wiringpi.digitalRead(button))
     if wiringpi.digitalRead(button):
         print("killed")
         subprocess.call(['pkill -f rfid_read.py'],shell = True)
