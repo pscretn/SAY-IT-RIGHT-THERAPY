a0e8b7b1164079cf760ded48be1d5edcdb7b4d0d
PAUL SABU <paulsabu.123@gmail.com>
2022-04-04 10:56:19 +0530

    python code

--- testcodes/keyboard_input.py
+++ testcodes/keyboard_input.py
@@ -22,6 +22,58 @@ def KeyPress(key):
         val+= '9' 
     elif key == KeyCode(char='0') or hasattr(key, 'vk') and key.vk == 96:
         val+= '0'
+    elif key == KeyCode(char='q'):
+        val+= 'q'
+    elif key == KeyCode(char='w'):
+        val+= 'w'  
+    elif key == KeyCode(char='e'):
+        val+= 'e'  
+    elif key == KeyCode(char='r'):
+        val+= 'r'  
+    elif key == KeyCode(char='t'):
+        val+= 't'  
+    elif key == KeyCode(char='y'):
+        val+= 'y'  
+    elif key == KeyCode(char='u'):
+        val+= 'u'  
+    elif key == KeyCode(char='i'):
+        val+= 'i'  
+    elif key == KeyCode(char='o'):
+        val+= 'o'   
+    elif key == KeyCode(char='p'):
+        val+= 'p'
+    elif key == KeyCode(char='a'):
+        val+= 'a'  
+    elif key == KeyCode(char='s'):
+        val+= 's'  
+    elif key == KeyCode(char='d'):
+        val+= 'd'  
+    elif key == KeyCode(char='f'):
+        val+= 'f'  
+    elif key == KeyCode(char='g'):
+        val+= 'g'  
+    elif key == KeyCode(char='h'):
+        val+= 'h'  
+    elif key == KeyCode(char='j'):
+        val+= 'j'  
+    elif key == KeyCode(char='k'):
+        val+= 'k' 
+    elif key == KeyCode(char='l'):
+        val+= 'l'
+    elif key == KeyCode(char='z'):
+        val+= 'z'  
+    elif key == KeyCode(char='x'):
+        val+= 'x'  
+    elif key == KeyCode(char='c'):
+        val+= 'c'  
+    elif key == KeyCode(char='v'):
+        val+= 'v'  
+    elif key == KeyCode(char='b'):
+        val+= 'b'  
+    elif key == KeyCode(char='n'):
+        val+= 'n'  
+    elif key == KeyCode(char='m'):
+        val+= 'm'  
     elif key == Key.space:
         val+= ' '
     elif key == Key.enter:
@@ -33,3 +85,4 @@ def KeyOut():
     with Listener(on_press = KeyPress) as listener:   
         listener.join()
     return val
+
